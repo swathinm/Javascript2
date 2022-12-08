44aea18fca843363f26f484fd065ce8358817e89
swathi <swathinm13@gmail.com>
2022-12-08 16:23:34 +0530

    okay

---
+++ dom/fatarrow/four.js
@@ -0,0 +1,18 @@
+/* function wish(){
+  console.log("gm")
+}
+wish()
+function wish(){
+  console.log("gn")
+}
+wish() */
+
+var wish = () => {
+  console.log("gm")
+}
+wish();
+var wish = () => {
+  console.log("gn")
+}
+wish();
+
